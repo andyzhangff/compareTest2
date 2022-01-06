4d36fd4f11f0d14277014f0c8f6813331c3d6275
andyzhangff <m13816475399@gmail.com>
2022-01-06 17:40:23 +0800

    Initial commit

---
+++ src/main/kotlin/Title.kt
@@ -0,0 +1,11 @@
+class Title(
+    val awsS3FolderName: String,
+    val category: List<String>,
+    val description: String,
+    val image: String,
+    val imageFileName: String,
+    val owner: String,
+    val recipeId: Int,
+    val time: Int,
+    val title: String,
+)
